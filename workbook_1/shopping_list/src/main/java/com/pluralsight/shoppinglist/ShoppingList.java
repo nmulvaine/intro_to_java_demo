db73e5f341a6d13e04e9085150ae5141fca67924
Nicholas Mulvaine <nmulvaine@icloud.com>
2024-09-26 10:15:10 -0400

    Shopping List added

---
+++ workbook_1/shopping_list/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
@@ -0,0 +1,12 @@
+package com.pluralsight.shoppinglist;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Shopping List");
+        String [] itemList = {
+                "Apples","Soup","Crackers","Cheese","Coke","Watermelon","Milk","Steak","Sugar","Oranges"
+        };
+        for (String i: itemList) System.out.println(i);
+
+    }
+}
